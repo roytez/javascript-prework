593684fc24b14dc742cf1000daba1069dcfe3c51
Mateusz Krasinkiewicz <mateuszkrasinkiewicz@gmail.com>
2019-07-30 13:28:49 +0200

    Add javascript code

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var computerMove, playerMove;
+computerMove = 'kamień';
+playerMove = 'papier ';
+printMessage(playerMove + 'Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
