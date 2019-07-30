a3cbe8baa8f6f440749ad5c3880e1a167036c7a1
Mateusz Krasinkiewicz <mateuszkrasinkiewicz@gmail.com>
2019-07-30 16:20:09 +0200

    Add computerMove & playerMove

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
 var computerMove, playerMove;
-computerMove = 'kamień';
 playerMove = 'papier ';
-printMessage(playerMove + 'Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove);
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
+
