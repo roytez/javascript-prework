aa51e77badcbfc4171aee0d723dd51a545f63fb3
Mateusz Krasinkiewicz <mateuszkrasinkiewicz@gmail.com>
2019-08-01 13:48:50 +0200

    Add playerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//losowanie ruchu komputera //
+
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -11,3 +13,20 @@ if (randomNumber == '1') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+// ustalenie ruchu gracza //
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'kamień';
+  printMessage('błędny wybór');
+}
+printMessage('Twój ruch: ' + playerMove);
